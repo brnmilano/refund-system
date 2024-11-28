f1109fc72e68dd1a309f87dceb8c32a5433b9349
Bruno <brn.milano@hotmail.com>
2024-11-27 21:07:30 -0300

    feat: construção do html e css - criação da função formattedInput

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Teste");
+const currencyInput = document.querySelector("#amount");
+
+/**
+ * @description Função que formata o input para aceitar apenas números.
+ *
+ * @param {*} inputValue - valor do input.
+ * @returns {string} - retorna o valor do input formatado.
+ */
+function formattedInput(inputValue) {
+  return inputValue.value.replace(/[^0-9.]/g, "").replace(/(\.*?)\..*/g, "$1");
+}
+
+/**
+ * @description Evento que formata o input ao digitar.
+ */
+currencyInput.oninput = () => {
+  currencyInput.value = formattedInput(currencyInput);
+
+  console.log(currencyInput.value);
+};
+
+try {
+  console.log("try");
+} catch (error) {
+  console.log("catch");
+  console.log(error);
+}
